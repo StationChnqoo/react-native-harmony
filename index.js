c82988010339c015bfc0b42c720f0933fe9da628
陈桥驿站 <i@cctv3.net>
2021-07-02 17:26:32 +0800

    🍺 init react-native-harmony 🎁  & 🎉 npm publish Okay ✌️

---
+++ index.js
@@ -0,0 +1,7 @@
+import Closure from "./state/Closure";
+
+const Harmony = {
+  Closure
+}
+
+module.exports = Harmony
---
+++ state/Closure.js
@@ -0,0 +1,40 @@
+var Store = (function () {
+  var StoreClass = function () {};
+  StoreClass.prototype = {
+    constructor: StoreClass,
+    // 初始化 state
+    initState: function () {
+      this.state = {};
+    },
+    // 获取当前实例的 state
+    getState: function () {
+      return this.state;
+    },
+    // Get or set
+    set: function (key, value) {
+      this.state[key] = value;
+    },
+    get: function (key, defaultValue) {
+      // 如果 state 里面没有缓存，则缓存 state[key] = defaultValue
+      if (!this.state.hasOwnProperty(key)) {
+        this.set(key, defaultValue);
+      }
+      return this.state[key];
+    },
+  };
+
+  // StoreClass 实例
+  var instance = null;
+  return {
+    getInstance: function () {
+      if (instance == null) {
+        instance = new StoreClass();
+        // 对象被创建的时候，初始化 state = {}
+        instance.initState();
+      }
+      return instance;
+    },
+  };
+})();
+
+export default Store;
