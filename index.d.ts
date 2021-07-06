30bf5b09d3f2b919f5231352a0ac2debe78107a0
陈桥驿站 <i@cctv3.net>
2021-07-06 09:39:11 +0800

    🛠 修改被遗忘的 index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+import React, {useEffect} from 'react';
+import Closure from "./src/state/Closure";
+import Colors from "./src/x/Colors";
+import Button, { ButtonProps } from './src/component/Button'
+
+export = {
+    Button
+}
